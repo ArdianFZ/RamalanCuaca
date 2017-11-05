577af4d0ad023ff0dd305ff528404224809c66a2
Ardiasnyah <ardia667@gmail.com>
2017-11-05 20:41:43 +0700

    menabahkan java

--- app/src/main/java/com/code/pali/ramalancuaca/MainActivity.java
+++ app/src/main/java/com/code/pali/ramalancuaca/MainActivity.java
@@ -2,12 +2,44 @@ package com.code.pali.ramalancuaca;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_Minggu)
+    TextView hariYngDiTampilkan;
+
+    @BindView(R.id.ImageMatahari)
+    ImageView gambarYangDitapilkan;
+
+    @BindView(R.id.textHujan)
+    TextView tapilaknCuaca;
+
+    @BindView(R.id.text17C)
+    TextView tampilanSuhu;
+
+    //bending
+    //@BindView(R.id.weather_List)
+    //RecyclerView weatherList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        hariYngDiTampilkan.setText("Mari MInggu");
+        tampilanSuhu.setText("18C");
+        tampilanSuhu.setText("Dingin Sangat");
+        gambarYangDitapilkan.setImageResource(R.mipmap.sun);
+
     }
 }
